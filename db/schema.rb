31e188888c5aee0dd4041e1bb3426119d39400cc
Victorien Brun <b.victorien@hotmail.fr>
2020-11-19 17:50:19 +0100

    Add controller doses & reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_164147) do
+ActiveRecord::Schema.define(version: 2020_11_19_164730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,14 @@ ActiveRecord::Schema.define(version: 2020_11_19_164147) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "cocktail_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+  end
+
+  add_foreign_key "reviews", "cocktails"
 end
